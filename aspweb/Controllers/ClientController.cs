41305a1ee835ccc10a8cf3bc96043bc728bf1e5b
Phuong Nguyen Viet <nguyenvietphuong271100@gmail.com>
2021-12-29 17:59:40 +0700

    Display product and category from DB

---
+++ aspweb/Controllers/ClientController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using aspweb.Models;
+
+namespace aspweb.Controllers
+{
+    public class ClientController : Controller
+    {
+        CosmesticShopDB db = new CosmesticShopDB();
+        // GET: Client
+        public ActionResult Index()
+        {
+            var new_products = db.tbl_products.OrderByDescending(a => a.price).Take(4).ToList();
+            return View(new_products);
+        }
+        public ActionResult ProductGrid()
+        {
+            var all_product = db.tbl_products.Select(p => p).ToList();
+            var all_category = db.tbl_category.Select(p => p).ToList();
+            Entity entity = new Entity();
+            entity._Products = all_product;
+            entity._Categories = all_category;
+            return View(entity);
+        }
+        public ActionResult Blog()
+        {
+            return View();
+        }
+        public ActionResult BlogDetail()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult ProductDetail(int id)
+        {
+            var product = from p in db.tbl_products
+                          where p.id == id
+                          select p;
+            return View(product.Single());
+        }
+    }
+}
---
+++ aspweb/Models/Entity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace aspweb.Models
+{
+    public class Entity
+    {
+        public List<tbl_products> _Products { get; set; }
+        public List<tbl_category> _Categories { get; set; }
+    }
+}
